fc069159e83e97a542d25ca06de06f90f6fa4e86
sayan112 <sayanmaitraj5@gmail.com>
2021-11-13 17:43:49 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,9 @@
+export default  class game {
+constructor() {
+console.log("hey there bro ");
+this.board= new Array(9).fill(null);
+this.turn(X);
+// console.log(this.board);
+// [null, null, null, null, null, null, null, null, null]
+}
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+//  import game from "./game.js";
+import game from "./game.js"
+//  import gameview
+let Game= new game();
+console.log("hello");
