a0e4ef3da23f652a462073b6ddd65bc8d42f4970
sayan112 <sayanmaitraj5@gmail.com>
2021-11-13 21:33:21 +0530

    2nd commit

--- game.js
+++ game.js
@@ -1,9 +1,32 @@
 export default  class game {
+
+
 constructor() {
 console.log("hey there bro ");
-this.board= new Array(9).fill(null);
-this.turn(X);
+ 
+this.turn = "X";
+this.board= new Array(9).fill(null); 
 // console.log(this.board);
 // [null, null, null, null, null, null, null, null, null]
 }
+
+ nextTurn()
+//   here if this.turn  is X then  next turn will be O 
+{
+     if(this.turn== "X")
+     {
+          this.turn="O" ; 
+
+     }
+     else{
+          this.turn="X" ; 
+     }
+}
+makeMove(i)
+{
+ this.board[i]=this.turn;
+}
+
+
+
 }
--- main.js
+++ main.js
@@ -2,4 +2,9 @@
 import game from "./game.js"
 //  import gameview
 let Game= new game();
-console.log("hello");
+// console.log("hello");
+console.log("My turn is", Game.turn);
+//  ok so now its time to understand :)
+//   at line no 4 , we create a variable Game , which calls a function new game  which triggers game.js , 
+
+
