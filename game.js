c3dfd6f0a2885120da3af3265a4b28c5ced62908
sayan112 <sayanmaitraj5@gmail.com>
2021-11-19 12:59:51 +0530

    3rd-commit

--- game.js
+++ game.js
@@ -24,7 +24,37 @@ this.board= new Array(9).fill(null);
 }
 makeMove(i)
 {
+     if(this.board[i])
+{
+     return;
+}
  this.board[i]=this.turn;
+let winningcombination = this.findwinningcombinations();
+console.log("this is winnng combination ",winningcombination);
+}
+
+findwinningcombinations()
+{
+    const winningcombinations =[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2]
+    ]
+     for(const combination of winningcombinations)
+     {
+          // console.log(combination);
+          const[a,b,c]=combination;
+           if(this.board[a] && this.board[a]==this.board[b] && this.board[a]==this.board[c])
+           {
+                return combination;
+           }
+     }
+     return null;
 }
 
 
--- main.js
+++ main.js
@@ -3,29 +3,49 @@ import game from "./game.js"
 import gameview from "./gameview.js"
 //  import gameview
 let Game= new game();
-let Gameview= new gameview();
-Gameview.updateThegameboard(Game);
-// console.log("hello");
-console.log("My turn is", Game.turn);
-Game.nextTurn();
-console.log("My turn is", Game.turn);
-Game.makeMove(8);
-Game.nextTurn();
-Game.makeMove(1);
-Game.nextTurn();
-Game.makeMove(2);
-console.log(Game.board);
-Gameview.updateThegameboard(Game);
-
 
 //  ok so now its time to understand :)
 //   at line no 4 , we create a variable Game , which calls a function new game  which triggers game.js , 
 
 
+let Gameview= new gameview();
+// Gameview.updateThegameboard(Game);
+// // console.log("hello");
+// console.log("My turn is", Game.turn);
+// Game.nextTurn();
+// //  through .nextrun we acutllay said that  now the next player  have the controle 
+// console.log("My turn is", Game.turn);
+// Game.makeMove(8);
+// Game.nextTurn();
+// Game.makeMove(1);
+// Game.nextTurn();
+// Game.makeMove(2);
+// console.log(Game.board);
+// Gameview.updateThegameboard(Game);
 
 
 
+let tiles= document.querySelectorAll(".board-tile")
+// console.log(tiles);
+tiles.forEach((tile)=>
+{
+    tile.addEventListener("click",()=>{
+        // console.log('tile clicked');
+        onTileclick(tile.dataset.index);
+    })
+})
 
+
+
+function onTileclick(i) {
+    // we have to make a move first 
+    Game.makeMove(i);
+
+    Gameview.updateThegameboard(Game); 
+    //  then it will update the board and next player can play now , its next turn
+
+    Game.nextTurn();
+}
 
 
 
